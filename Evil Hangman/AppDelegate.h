ac954405d2fb090d35c8c809d3aade627827404f
Rick Buchter <rick.buchter@student.uva.nl>
2014-11-13 08:40:01 +0100

    Initial Commit

---
+++ Evil Hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Evil Hangman
+//
+//  Created by Rick Buchter on 13-11-14.
+//  Copyright (c) 2014 Rick Buchter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Evil Hangman/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Evil Hangman
+//
+//  Created by Rick Buchter on 13-11-14.
+//  Copyright (c) 2014 Rick Buchter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
